8945ccb2fa239135784112e6387e6a2abef861bb
teodor1006 <tedi_montana@abv.bg>
2024-01-29 13:00:16 +0200

    added 2 installation scripts (kubectl + awscli)

--- Jenkins-Terraform/install_jenkins.sh
+++ Jenkins-Terraform/install_jenkins.sh
@@ -28,3 +28,17 @@ echo "deb [signed-by=/usr/share/keyrings/trivy.gpg] https://aquasecurity.github.
 sudo apt-get update
 sudo apt-get install trivy -y
 
+# Install Kubectl 
+sudo apt update
+sudo apt install curl -y
+curl -LO https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
+
+# Install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt-get install unzip -y
+unzip awscliv2.zip
+sudo ./aws/install
+
+
